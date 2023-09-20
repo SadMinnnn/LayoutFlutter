892960fa8f62612303dffb848987ad3525199d52
SadMin <vegita796@gmail.com>
2023-09-20 10:39:59 -0500

    up

--- lib/main.dart
+++ lib/main.dart
@@ -15,79 +15,91 @@ class MyApp extends StatelessWidget {
             elevation: 5,
             child: Container(
               width: 500,
-              height: 450,
-              padding: EdgeInsets.all(8.0),
-              margin: EdgeInsets.all(25.0),
+              height: 550,
               child: SingleChildScrollView(
                 child: Column(
                   children: <Widget>[
-                    // Título, subtitulo y estrella
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'Oeschinen Lake Campground',
-                              style: TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
+                    Container(
+                      padding: EdgeInsets.all(8.0),
+                      margin: EdgeInsets.all(25.0),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    'Oeschinen Lake Campground',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 7),
+                                  Text(
+                                    'Kandersteg, Switzerland',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                      color: Colors.blueGrey,
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                            Text(
-                              'Kandersteg, Switzerland',
+                              Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.star,
+                                        color: Colors.yellow,
+                                      ),
+                                      SizedBox(width: 4),
+                                      Text(
+                                        '41',
+                                        style: TextStyle(
+                                          fontSize: 18.0,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          Container(
+                            width: 480,
+                            child: Text(
+                              'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+                              'Alps. Situated 1,578 meters above sea level, it is one of the '
+                              'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+                              'half-hour walk through pastures and pine forest, leads you to the '
+                              'lake, which warms to 20 degrees Celsius in the summer. Activities '
+                              'enjoyed here include rowing, and riding the summer toboggan run.',
                               style: TextStyle(
                                 fontSize: 16.0,
-                                color: Colors.blueGrey,
+                                color: Colors.black,
                               ),
                             ),
-                          ],
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Icon(
-                              Icons.star,
-                              color: Colors.yellow,
-                            ),
-                            SizedBox(width: 4),
-                            Text(
-                              '41',
-                              style: TextStyle(
-                                fontSize: 18.0,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 20),
-                    // Texto
-                    Container(
-                      width: 480,
-                      child: Text(
-                        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-                        'Alps. Situated 1,578 meters above sea level, it is one of the '
-                        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-                        'half-hour walk through pastures and pine forest, leads you to the '
-                        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-                        'enjoyed here include rowing, and riding the summer toboggan run.',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.black,
-                        ),
+                          ),
+                        ],
                       ),
                     ),
-                    SizedBox(height: 20),
-                    // Fondo de imagen
                     Container(
-                      height: 150.0,
+                      width: 600.0,
+                      height: 240.0, // Altura de la imagen de fondo
                       decoration: BoxDecoration(
                         image: DecorationImage(
                           image: NetworkImage(
                             'https://upload.wikimedia.org/wikipedia/commons/0/0d/Oeschinen_Lake%2C_Kandersteg%2C_Switzerland_%28Unsplash%29.jpg',
                           ),
-                          fit: BoxFit.fill,
+                          fit: BoxFit
+                              .fill, // Ajuste de la imagen para llenar el espacio
                         ),
                       ),
                     ),
